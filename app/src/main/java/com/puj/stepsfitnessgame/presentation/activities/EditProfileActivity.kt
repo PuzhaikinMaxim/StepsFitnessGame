13eb40004065bf0db057a30a3a414bd23613f8d5
PuzhaikinMaxim <56976303+PuzhaikinMaxim@users.noreply.github.com>
2023-05-27 16:57:37 +0300

    Minor changes

---
+++ app/src/main/java/com/puj/stepsfitnessgame/presentation/activities/EditProfileActivity.kt
@@ -0,0 +1,7 @@
+package com.puj.stepsfitnessgame.presentation.activities
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+
+class EditProfileActivity: AppCompatActivity() {
+}
